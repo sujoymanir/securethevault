2b7f0056745fd7b320bf71443f6edc06f5c1bc5c
Sujoy Manir <sujoymanir@gmail.com>
2024-07-22 13:31:48 -0400

    Finalized Copy of Passcode

---
+++ script.js
@@ -0,0 +1,18 @@
+/* 
+
+1. Create three different variables each  with their own operation.
+2. Make sure each var equates to the codes in the "secret code"
+3. I will add comments throughout the code in script.js.
+4. Create an alert to populate on the browser (i.e dialog box)
+
+*/
+
+const num1= 5+1+2+1+1;
+const num2= 10 *4;
+const num3= 70-31;
+
+const newString3= `The Secret Combination is ${num1},${num2},${num3}`
+
+console.log(newString3);
+
+alert(newString3)
